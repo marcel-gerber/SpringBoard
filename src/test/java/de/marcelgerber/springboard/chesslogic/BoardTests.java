4705e4018a55e8fc7156974909b11b1264fe053c
Marcel Gerber <marcelgerber9@gmail.com>
2024-10-25 00:08:45 +0200

    Added BoardTests

---
+++ src/test/java/de/marcelgerber/springboard/chesslogic/BoardTests.java
@@ -0,0 +1,39 @@
+package de.marcelgerber.springboard.chesslogic;
+
+import de.marcelgerber.springboard.core.chesslogic.Board;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTests {
+
+    private Board board;
+
+    private static final String[] FEN_STRINGS = new String[] {
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+            "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+            "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+            "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 1",
+            "8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1",
+            "r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1",
+            "r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1",
+            "8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1"
+    };
+
+    @BeforeEach
+    void setup() {
+        board = new Board();
+    }
+
+    @Test
+    public void setAndGetFen() {
+        for(String fen : FEN_STRINGS) {
+            board.setFen(fen);
+            assertEquals(fen, board.getFen(), "setAndGetFen Test failed!");
+        }
+    }
+
+}
