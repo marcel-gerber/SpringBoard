050b16230532a759d779b30502c69f8c1de51c27
Marcel Gerber <marcelgerber9@gmail.com>
2024-10-27 21:45:23 +0100

    Added GameService

--- src/main/java/de/marcelgerber/springboard/core/GameService.java
+++ src/main/java/de/marcelgerber/springboard/core/GameService.java
@@ -1,8 +1,21 @@
 package de.marcelgerber.springboard.core;
 
+import de.marcelgerber.springboard.persistence.GameRepository;
+import de.marcelgerber.springboard.persistence.documents.GameDocument;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
+
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public GameDocument createGame() {
+        GameDocument gameDocument = new GameDocument();
+        return gameRepository.save(gameDocument);
+    }
 
 }
