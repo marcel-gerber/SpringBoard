9242f7217584177950f75176a53a9f4a3f90b304
Marcel Gerber <marcelgerber9@gmail.com>
2024-10-31 22:06:05 +0100

    Added getChar()

--- src/main/java/de/marcelgerber/springboard/core/chesslogic/pieces/PieceType.java
+++ src/main/java/de/marcelgerber/springboard/core/chesslogic/pieces/PieceType.java
@@ -14,6 +14,12 @@ public enum PieceType {
     KING,
     NONE;
 
+    /**
+     * Returns an instance of a Piece with the provided color
+     *
+     * @param color Color
+     * @return Piece
+     */
     public Piece getPiece(Color color) {
         return switch (this) {
             case PieceType.KNIGHT -> new Knight(color);
@@ -21,6 +27,21 @@ public enum PieceType {
             case PieceType.ROOK -> new Rook(color);
             case PieceType.QUEEN -> new Queen(color);
             default -> NullPiece.getInstance();
+        };
+    }
+
+    /**
+     * Returns the pieceTypes' character (used for determine promotion piece type)
+     *
+     * @return char
+     */
+    public char getChar() {
+        return switch (this) {
+            case PieceType.KNIGHT -> 'n';
+            case PieceType.BISHOP -> 'b';
+            case PieceType.ROOK -> 'r';
+            case PieceType.QUEEN -> 'q';
+            default -> ' ';
         };
     }
 
