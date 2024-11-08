03a24d14c47dcdadbf8df1ea86770b2ddc77422c
Marcel Gerber <marcelgerber9@gmail.com>
2024-11-08 22:58:20 +0100

    Added standard exceptions to GlobalExceptionHandler

--- src/main/java/de/marcelgerber/springboard/api/GlobalExceptionHandler.java
+++ src/main/java/de/marcelgerber/springboard/api/GlobalExceptionHandler.java
@@ -3,16 +3,42 @@ package de.marcelgerber.springboard.api;
 import de.marcelgerber.springboard.api.dto.ErrorResponse;
 import de.marcelgerber.springboard.exceptions.GameNotFoundException;
 import de.marcelgerber.springboard.exceptions.IllegalMoveException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * Class for handling custom errors
  */
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        String error = "No valid JSON format";
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), error);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        String error = ex.getBindingResult().getAllErrors().getFirst().getDefaultMessage();
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), error);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 
     /**
      * Method for handling GameNotFoundException
