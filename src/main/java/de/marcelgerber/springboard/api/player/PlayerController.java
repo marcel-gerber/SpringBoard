ae18d9cf521e1ff407d9b27fac54c91bd8d09633
Marcel Gerber <marcelgerber9@gmail.com>
2024-11-12 00:51:37 +0100

    Continued working on PlayerController (WIP)

--- src/main/java/de/marcelgerber/springboard/api/player/PlayerController.java
+++ src/main/java/de/marcelgerber/springboard/api/player/PlayerController.java
@@ -1,14 +1,21 @@
 package de.marcelgerber.springboard.api.player;
 
+import de.marcelgerber.springboard.core.player.PlayerService;
+import de.marcelgerber.springboard.persistence.documents.PlayerDocument;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/players")
 public class PlayerController {
+
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
 
     /**
      * GET /api/players <br>
@@ -17,23 +24,35 @@ public class PlayerController {
      * @return ResponseEntity
      */
     @GetMapping
-    public ResponseEntity<?> getAllPlayers() {
-        return ResponseEntity.ok("GET /api/players");
+    public ResponseEntity<List<PlayerDocument>> getAllPlayers() {
+        return ResponseEntity.ok(playerService.getAllPlayers());
     }
 
     /**
-     * POST /api/players <br>
+     * GET /api/players/{playerId} <br>
+     * Retrieves all players
+     *
+     * @return ResponseEntity
+     */
+    @GetMapping("/{playerId}")
+    public ResponseEntity<PlayerDocument> getPlayerById(@PathVariable String playerId) {
+        PlayerDocument playerDocument = playerService.getPlayerById(playerId);
+        return ResponseEntity.ok(playerDocument);
+    }
+
+    /**
+     * POST /api/players/register <br>
      * Register a new player
      *
      * @return ResponseEntity
      */
     @PostMapping
     public ResponseEntity<?> registerPlayer() {
-        return ResponseEntity.ok("POST /api/players");
+        return ResponseEntity.ok("POST /api/players/register");
     }
 
     /**
-     * POST /api/players <br>
+     * POST /api/players/login <br>
      * Register a new player
      *
      * @return ResponseEntity
