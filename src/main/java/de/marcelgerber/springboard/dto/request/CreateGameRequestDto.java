4d65d665e79484a43e3a3089a5d8a28b38e9cde3
Marcel Gerber <marcelgerber9@gmail.com>
2024-11-13 23:21:05 +0100

    Removed playername from CreateGameRequestDto

--- src/main/java/de/marcelgerber/springboard/dto/request/CreateGameRequestDto.java
+++ src/main/java/de/marcelgerber/springboard/dto/request/CreateGameRequestDto.java
@@ -1,13 +1,9 @@
 package de.marcelgerber.springboard.dto.request;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class CreateGameRequestDto {
-
-    @NotEmpty(message = "Key 'playername' is required")
-    private String playername;
 
     private String color;
 
