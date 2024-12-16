0e2c7d4cd95af1bc67a29c97dd072d0b2fd70a56
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-16 23:11:25 +0100

    Added SessionResponseDto

---
+++ src/main/java/de/marcelgerber/springboard/dto/response/SessionResponseDto.java
@@ -0,0 +1,12 @@
+package de.marcelgerber.springboard.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SessionResponseDto {
+
+    private boolean valid;
+
+}
