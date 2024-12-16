fc98c24ec25010925fdd9cc9f33770e650435d11
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-16 23:11:36 +0100

    Added LogoutResponseDto

---
+++ src/main/java/de/marcelgerber/springboard/dto/response/LogoutResponseDto.java
@@ -0,0 +1,12 @@
+package de.marcelgerber.springboard.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LogoutResponseDto {
+
+    private String message;
+
+}
